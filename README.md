276cea764bc5aef570ba759713854e10855f6f19
reyniel26 <rmescamillas26@gmail.com>
2021-09-21 14:22:57 +0800

    BLEEPY-63 Updated app.py
    Added JWT for login

--- README.md
+++ README.md
@@ -7,15 +7,16 @@ Bleepy  Server is the web version of python program can block tagalog and englis
    - Bleepy_UI should be save in static folder
 
 ## Python modules needed
-1. vosk-api
+1. vosk-api (0.3.30)
    - Offline speech recognition 
    - https://github.com/alphacep/vosk-api
 
-2. alt-profanity-check
+2. alt-profanity-check (0.24.2)
    - Profanity checker in string that uses machine learning
    - https://pypi.org/project/alt-profanity-check/
    - https://github.com/dimitrismistriotis/profanity-check
 
-3. flask
-4. flask-mysqldb
+3. flask (2.0.1)
+4. mysql-connector-python (8.0.26)
 5. passlib
+6. pyjwt (2.1.0)
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #================================================== Imports
 #Flask
-from flask import Flask, render_template, flash, redirect, url_for, request
+from typing import Dict
+from flask import Flask, render_template, flash, redirect, url_for, request, make_response
 #For decorator
 from functools import wraps 
+#JWT
+import jwt
 
 #Model for DB
 from model import Model
@@ -11,7 +14,7 @@ from bleepy.bleepy import VideoFile, AudioFile, SpeechToText, ProfanityExtractor
 
 #================================================== Configs
 app = Flask(__name__)
-app.secret_key = 'bleepy_server' #Set the secret_key
+app.secret_key = 'bL33py_sE12v3r' #Set the secret_key
 
 #================================================== Objects
 db = Model()
@@ -31,7 +34,59 @@ def testConn(f):
             return redirect(url_for('error'))
     return wrap
 
+def generateToken(**kwargs:str):
+    """
+    To generate token, specify the key and values
+    Example: key_one="the value_one",key_two="value_two" 
+    """
+    token = jwt.encode(kwargs,app.secret_key,algorithm="HS256")
+    return token
 
+def getTokenValues(token:str):
+    """
+    Return Dict value of JWT or return None if the token is invalid
+    """
+    try:
+        values = jwt.decode(token,app.secret_key,algorithms=["HS256"])
+        return values
+    except:
+        return None
+
+def authentication(f):
+    """
+    Check the authentication token of the user
+    if the token not exist, its expires or credentials not set yet
+    if the token not have values or has invalid secretkey, its invalid
+    if the token not have authid, its invalid
+    """
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        cookies = request.cookies
+        token = cookies.get("authtoken")
+        if not token:
+            #The token not exist
+            flash("Invalid Authentication. Please try to login","warning")
+            return redirect(url_for('signin'))
+        
+        tokenvalues = getTokenValues(token)
+
+        if not tokenvalues:
+            #The token values is None
+            flash("Invalid Authentication","danger")
+            return redirect(url_for('signin'))
+        
+        try:
+            acc_id = tokenvalues["authid"]
+        except:
+            #The token doenst contain authid
+            flash("Invalid Authentication","danger")
+            return redirect(url_for('signin'))
+        
+        # Query the acc_id
+        # if not exist return error
+
+        return f(*args, **kwargs)
+    return wrap
 
 #================================================== Routes 
 
@@ -64,15 +119,37 @@ def signup():
 @testConn
 def signin():
     if request.method == "POST":
+        email = request.form["email"]
+        pwd = request.form["pwd"]
+        remember = request.form.getlist("remember")
+        #remember 30days or else remember for 5mins
+        max_age = (60*60*24*30) if remember else (60*5)  
+
+        if email != "sampleemail@gmail.com":
+            flash('Wrong Email', 'danger')
+            return render_template('signin.html')
+        if pwd != "password":
+            flash('Wrong Password', 'danger')
+            return render_template('signin.html')
+
+        #set response
+        res = make_response(redirect(url_for('dashboard')))
+        #set cookie token
+        res.set_cookie("authtoken", 
+            value = generateToken(authid="1",validuntil=max_age),
+            max_age = max_age
+            )
+
         flash('You are now logged in ', 'success')
-        return redirect(url_for('dashboard'))
+        return res
     return render_template('signin.html')
 
-#Pages that can only be access when logged in
+#Pages and routes that can only be access when logged in
 
 #Settings Page
 @app.route('/settings')
 @testConn
+@authentication
 def settings():
     
     return render_template('settings.html')
@@ -80,9 +157,25 @@ def settings():
 #Dashboard Page
 @app.route('/dashboard')
 @testConn
+@authentication
 def dashboard():
     
     return render_template('dashboard.html')
+
+#Log out route
+@app.route('/logout')
+@testConn
+@authentication
+def logout():
+    #set response
+    res = make_response(redirect(url_for('signin')))
+    #set cookie token | expires = 0 to delete cookie
+    res.set_cookie("authtoken", 
+        value = "",
+        expires=0
+        )
+    flash('You are now logged out ', 'success')
+    return res
 
 #================================================== Run APP 
 if __name__ == '__main__':
